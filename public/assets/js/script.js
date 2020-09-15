a3de0d3d251a96279879d3ce83b006d5c5ad433e
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2020-09-14 23:13:56 -0300

    v1

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,16 +5,12 @@ const main = document.querySelector(".main");
 self.addEventListener("load", function (evento) {
   trabalhos.forEach((trabalho) => {
     let card = `
-        <div class="row">
-            <div class="column">
-                <a href="${trabalho.link_video}" class="link_video" target="_blank">
-                    <div class="card">
-                        <img src="public/assets/image/youtube.jpg">
-                        <p class="titulo">${trabalho.titulo}</p>
-                    </div>
-                </a>
+        <a href="${trabalho.link_video}" class="link_video" target="_blank">
+            <div class="card">
+                <img src="public/assets/image/youtube.jpg">
+                <p class="titulo">${trabalho.titulo}</p>
             </div>
-        </div>
+        </a>
     `;
 
     main.innerHTML += card;
