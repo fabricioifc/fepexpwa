29f71b3d5ca166bd741599225e550b4b98e3be08
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2020-09-15 09:31:05 -0300

    v1

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,18 +1,30 @@
 import { trabalhos } from "./trabalhos.js";
 
 const main = document.querySelector(".main");
+const tamanho = 4;
+const trabalhos_em_partes = new Array(Math.ceil(trabalhos.length / tamanho))
+  .fill()
+  .map((_) => trabalhos.splice(0, tamanho));
 
 self.addEventListener("load", function (evento) {
-  trabalhos.forEach((trabalho) => {
-    let card = `
+  trabalhos_em_partes.forEach((itens) => {
+    let html = "<div class='row'>";
+
+    itens.forEach((trabalho) => {
+      let card = `
+      <div class="column">
         <a href="${trabalho.link_video}" class="link_video" target="_blank">
             <div class="card">
                 <img src="public/assets/image/youtube.jpg">
                 <p class="titulo">${trabalho.titulo}</p>
             </div>
         </a>
+        </div>
     `;
+      html += card;
+    });
+    html += "</div>";
 
-    main.innerHTML += card;
+    main.innerHTML += html;
   });
 });
