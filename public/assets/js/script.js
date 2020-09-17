84b8ce9bef16e2fc8395bf428bbb593d0eade4a9
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2020-09-17 09:33:49 -0300

    v1

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -11,8 +11,8 @@ self.addEventListener("load", function (evento) {
               <p class="small-text">${trabalho.titulo}</p>
             </div>
             <div class="card-action">
-              <a href="${trabalho.link_trabalho}" target="_blank" title="Trabalho Escrito"><i class="material-icons orange-text">picture_as_pdf</i></a>
-              <a href="${trabalho.link_video}" target="_blank" title="Apresentação"><i class="material-icons red-text">play_circle_filled</i></a>
+              <a href="${trabalho.link_trabalho}" title="Trabalho Escrito"><i class="material-icons orange-text">picture_as_pdf</i></a>
+              <a href="${trabalho.link_video}" title="Apresentação"><i class="material-icons red-text">play_circle_filled</i></a>
               <span class="badge blue darken-3 white-text">${trabalho.area}</span>
               <span class="badge indigo darken-3 white-text">${trabalho.modalidade}</span>
             </div>
