75743f4a7b91099f2b2afb324c75764da5c0befa
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2020-09-22 08:43:42 -0300

    v3

---
+++ public/assets/js/script.js
@@ -0,0 +1,28 @@
+import trabalhos from '../../data/trabalhos.js';
+
+const main = document.querySelector('div.trabalhos');
+
+self.addEventListener('load', function (evento) {
+    main.innerHTML = null;
+
+    trabalhos.forEach(trabalho => {
+
+        var card = `
+            <div class="col l4 m6 s12">
+                <div class="card small indigo darken-4">
+                    <div class="card-content white-text">
+                        <p class="small-text">${trabalho.titulo}</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="${trabalho.link_trabalho}" title="Trabalho Escrito"><i class="material-icons orange-text">picture_as_pdf</i></a>
+                        <a href="${trabalho.link_video}" title="Apresentação"><i class="material-icons red-text">play_circle_filled</i></a>
+                        <span class="badge blue darken-3 white-text">${trabalho.area}</span>
+                        <span class="badge indigo darken-3 white-text">${trabalho.modalidade}</span>
+                    </div>
+                </div>
+            </div>
+        `
+        main.innerHTML += card;
+    });
+
+});
